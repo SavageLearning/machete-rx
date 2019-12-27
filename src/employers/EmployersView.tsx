0770ba8d67cd4fd009b63a1d7235c33a90976c8b
jcii <jciispam@gmail.com>
2019-12-27 14:08:42 -0800

    working redux-query + openapi

--- src/employers/EmployersView.tsx
+++ src/employers/EmployersView.tsx
@@ -3,45 +3,24 @@ import { useSelector } from 'react-redux';
 import { useRequest } from 'redux-query-react';
 import * as client from 'machete-api-redux-query-es6-client';
 import { Employer, TypedQueryConfig } from 'machete-api-redux-query-es6-client';
-import { Update, UpdateStrategy, TransformStrategy } from 'redux-query';
-// interface IProps {}
-// import * as storyQueryConfigs from '../query-configs/stories';
 
-const myUpdateStrategy: UpdateStrategy<Employer[]> =  (prevValue: any, newValue: any) => {
-  console.log(prevValue, newValue);
-  return newValue; 
-}
-const myUpdate = { 'employers': myUpdateStrategy } as Update<{ employers: Employer[] }>;
-
-const myTransform: TransformStrategy<{ employers: Employer[] }, void> =  (body: any) => { 
-  const { data } = body;
-  const  employers = data as Employer[];
-  console.log(body);
-  return { employers }; 
+const getEmployers = (state: any) => { 
+  return state.entities.employers; 
 }
 
-const request: TypedQueryConfig<{ employers: Employer[] }, any> = {
-  transform: myTransform,
-  update: myUpdate
-
+const employersRequest: TypedQueryConfig<{employers: Employer[]}, any> = {
+  transform: (body: any) => ({ employers: body }),
+  update: {
+    employers: (oldValue: Employer[], newValue: Employer[]): Employer[] =>  newValue
+  }
 }
 
 export const EmployersView: FunctionComponent = () => {
-  // const [{ isPending }] = useRequest(storyQueryConfigs.itemRequest(props.itemId));
-  // const item = useSelector(state => storySelectors.getItem(state, props.itemId));
-
-  const [{ isPending }] = useRequest<{ employers: Employer[] }>(client.apiEmployerGet(request));
-  const itemm = useSelector((state: { entities: { employers: Employer[]}}) => {
-    console.log(state);
-    return state.entities.employers || [];
-  });
-
-  const foo = (item: Employer[]) => {
-    return JSON.stringify(item[0])
-  }
+  const [{ isPending }] = useRequest(client.apiEmployerGet(employersRequest));
+  const employers = useSelector(getEmployers) || [];
 
   return (<div>
     {isPending && "PENDING"}
-    {!isPending && foo(itemm)}
+    {!isPending && JSON.stringify(employers)}
   </div>);
 }
