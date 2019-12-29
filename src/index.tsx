d4c3733dec7270f9962045735ad899badbd9a6da
jcii <jciispam@gmail.com>
2019-12-29 15:18:26 -0800

    redux-query+openapi client+Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Provider as ReduxQueryProvider } from 'redux-query-react';
+import store  from './services/ReduxStore';
+
+export const getQueries = (state: any) => state.queries;
+
+ReactDOM.render(
+        <Provider store={store}>
+            <ReduxQueryProvider queriesSelector={getQueries}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+        </ReduxQueryProvider>
+        </Provider>
+
+    , document.getElementById('root') as HTMLElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
